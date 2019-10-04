14d77b9eb2d5e95b62793ee6ca8d91be72535110
Sjomullen <filip.sjogren@outlook.com>
2019-10-04 08:43:07 +0200

    Första array uppgiften

---
+++ Arrays uppgift/Arrays uppgift/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays_uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write five numbers");
+            int[] Number = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+              string inputString = Console.ReadLine();
+                Number[i] = int.Parse(inputString);
+            }
+            Console.WriteLine();
+            for(int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(Number[i] + " ");
+            }
+
+        }
+    }
+}
